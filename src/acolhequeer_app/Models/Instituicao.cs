92a8dad4d6d6783435c8c4b6ec620cbeac9c857d
Sara Guanais de Miranda Miranda <1494972@sga.pucminas.br>
2024-05-08 14:26:03 -0300

    teste

--- src/acolhequeer_app/Models/Instituicao.cs
+++ src/acolhequeer_app/Models/Instituicao.cs
@@ -9,51 +9,67 @@ public class Instituicao
     [Key]
     public int instituicao_id { get; set; }
 
+    [Display(Name = "Nome")]
     [Required(ErrorMessage = "Obrigatório informar o nome!")]
     public string nome { get; set; }
 
+    [Display(Name = "Insira o cnpj")]
     [Required(ErrorMessage = "Obrigatório informar o cnpj!")]
     public string cnpj { get; set; }
 
+    [Display(Name = "Insira o e-mail")]
     [Required(ErrorMessage = "Obrigatório informar o email!")]
     public string email { get; set; }
 
+    [Display(Name = "Insira o telefone")]
     [Required(ErrorMessage = "Obrigatório informar o telefone!")]
     public string telefone { get; set; }
 
+    [Display(Name = "Insira o nome")]
     [Required(ErrorMessage = "Obrigatório informar a rua!")]
     public string endereco_rua { get; set; }
 
+    [Display(Name = "Insira o bairro")]
     [Required(ErrorMessage = "Obrigatório informar o bairro!")]
     public string endereco_bairro { get; set; }
 
+    [Display(Name = "Insira a cidade")]
     [Required(ErrorMessage = "Obrigatório informar a cidade!")]
     public string endereco_cidade { get; set; }
 
+    [Display(Name = "Insira o estado")]
     [Required(ErrorMessage = "Obrigatório informar o estado!")]
     public string endereco_estado { get; set; }
 
+    [Display(Name = "Insira o numero")]
     [Required(ErrorMessage = "Obrigatório informar o numero!")]
     public int endereco_numero { get; set; }
 
+    [Display(Name = "Insira o CEP")]
     [Required(ErrorMessage = "Obrigatório informar o cep!")]
     public string endereco_cep { get; set; }
 
+    [Display(Name = "Insira a senha")]
     [Required(ErrorMessage = "Obrigatório informar a senha!")]
     public string senha { get; set; }
 
+    [Display(Name = "Insira a validação")]
     [Required(ErrorMessage = "Obrigatório informar a validação!")]
     public bool adm_validacao { get; set; }
 
+    [Display(Name = "Insira o pix")]
     [Required(ErrorMessage = "Obrigatório informar o pix!")]
     public string pix_doacao { get; set; }
 
+    [Display(Name = "Insira o numero de vags")]
     [Required(ErrorMessage = "Obrigatório informar o numero de vagas!")]
     public int n_vagas { get; set; }
 
+    [Display(Name = "Insira o atendimento")]
     [Required(ErrorMessage = "Obrigatório informar o atendimento!")]
     public bool bool_atd { get; set; }
 
+    [Display(Name = "Insira a disponibilidade")]
     [Required(ErrorMessage = "Obrigatório informar a disponibilidade!")]
     public int qtd_disponibilidade { get; set; }
 
--- src/acolhequeer_app/obj/Release/net8.0/acolhequeer_app.AssemblyInfo.cs
+++ src/acolhequeer_app/obj/Release/net8.0/acolhequeer_app.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("acolhequeer_app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f32bf7a93f8debcf1d27d01acef0146a572ae38")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c453d36c4a1d4af2dc8d4769fca07f377c17100")]
 [assembly: System.Reflection.AssemblyProductAttribute("acolhequeer_app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("acolhequeer_app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
