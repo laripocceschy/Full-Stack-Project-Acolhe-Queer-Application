b57c33cf9efa50c1aea7b2a04638ef9a20094def
Ana Laura F. B. Reis <alfbr@sga.pucminas.br>
2024-05-21 15:21:23 -0300

    Checkout

--- src/acolhequeer_app/obj/Debug/net8.0/acolhequeer_app.AssemblyInfo.cs
+++ src/acolhequeer_app/obj/Debug/net8.0/acolhequeer_app.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("acolhequeer_app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+274cf432e7efe9f5a36355dd32b45a9fcf1a5807")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+663d32e10d1eedadbc62960abcc6e70a44d57f7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("acolhequeer_app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("acolhequeer_app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
