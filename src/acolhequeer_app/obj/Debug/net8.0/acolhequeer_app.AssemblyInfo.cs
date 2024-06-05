4e35ac9f7c027aed3ced59e46a63710f9cb4f042
Bowie <analaurafbr@gmail.com>
2024-06-05 19:03:30 -0300

    changes

--- src/acolhequeer_app/obj/Debug/net8.0/acolhequeer_app.AssemblyInfo.cs
+++ src/acolhequeer_app/obj/Debug/net8.0/acolhequeer_app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("acolhequeer_app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f8eabdbbcb2d98c0637217b21abf6f6e82df998")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a4e5eea3a4502cd0b8f0543f793d6e30a4c412a")]
 [assembly: System.Reflection.AssemblyProductAttribute("acolhequeer_app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("acolhequeer_app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
