da1060a37de526ddeaa5b6ab2a53c23051136861
Bowie <analaurafbr@gmail.com>
2024-06-06 19:01:11 -0300

    commit com projeto

---
+++ acolhequeer/acolhequeer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace acolhequeer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
