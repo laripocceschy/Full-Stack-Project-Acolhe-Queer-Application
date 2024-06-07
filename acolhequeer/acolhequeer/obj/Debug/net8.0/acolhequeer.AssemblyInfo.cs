a4c27e360c709e28c1911ba395c3200740b609e9
Bowie <analaurafbr@gmail.com>
2024-06-07 07:36:25 -0300

    Adicionando o arquivo do projeto

--- acolhequeer/acolhequeer/obj/Debug/net8.0/acolhequeer.AssemblyInfo.cs
+++ acolhequeer/acolhequeer/obj/Debug/net8.0/acolhequeer.AssemblyInfo.cs
@@ -11,11 +11,11 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("80cbed15-8172-43a8-8246-87b49b1e953f")]
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("c2a9997c-9038-4163-bb69-d2d0c8bf64f4")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("acolhequeer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84b4a300a739336cb84c5881a802c9414ae5a776")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("acolhequeer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("acolhequeer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
